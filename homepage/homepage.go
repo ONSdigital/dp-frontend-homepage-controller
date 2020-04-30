d7d2c27e6552acde306d567425454c7887133fb8
Jon Dewi Jones <jondewijones@gmail.com>
2020-04-30 20:58:24 +0100

    Add goroutine for fetching and mapping data

--- homepage/homepage.go
+++ homepage/homepage.go
@@ -1,14 +1,17 @@
 package homepage
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
+	"sync"
 
 	"github.com/ONSdigital/dp-api-clients-go/renderer"
 	"github.com/ONSdigital/dp-api-clients-go/zebedee"
 	"github.com/ONSdigital/dp-frontend-homepage-controller/mapper"
 	model "github.com/ONSdigital/dp-frontend-models/model/homepage"
 	"github.com/ONSdigital/log.go/log"
+	"github.com/davecgh/go-spew/spew"
 )
 
 type MainFigure struct {
@@ -26,21 +29,32 @@ func Handler(rend renderer.Renderer, zcli *zebedee.Client) http.HandlerFunc {
 
 func handle(w http.ResponseWriter, req *http.Request, rend renderer.Renderer, zcli *zebedee.Client) {
 	ctx := req.Context()
-
 	mainFiguresList := getMainFiguresList()
-	var mappedMainFigures []model.MainFigure
 
+	var mappedMainFigures []model.MainFigure
+	var wg sync.WaitGroup
+	var mutex = &sync.Mutex{}
 	for _, value := range mainFiguresList {
-		zebResp, err := zcli.GetTimeseriesMainFigure(ctx, "", value.uri)
-		if err != nil {
-			log.Event(ctx, "error getting timeseries data", log.Error(err))
-			http.Error(w, "error getting timeseries data", http.StatusBadRequest)
-		}
-		mappedMainFigure := mapper.MainFigure(ctx, value.datePeriod, zebResp)
-		mappedMainFigures = append(mappedMainFigures, mappedMainFigure)
+		wg.Add(1)
+		go func(ctx context.Context, zcli *zebedee.Client, value MainFigure) {
+			defer wg.Done()
+			zebResp, err := zcli.GetTimeseriesMainFigure(ctx, "", value.uri)
+			if err != nil {
+				log.Event(ctx, "error getting timeseries data", log.Error(err))
+				http.Error(w, "error getting timeseries data", http.StatusBadRequest)
+			}
+			mappedMainFigure := mapper.MainFigure(ctx, value.datePeriod, zebResp)
+			mutex.Lock()
+			defer mutex.Unlock()
+			mappedMainFigures = append(mappedMainFigures, mappedMainFigure)
+			return
+		}(ctx, zcli, value)
 	}
+	wg.Wait()
 
 	m := mapper.Homepage(ctx, mappedMainFigures)
+
+	spew.Dump(m)
 
 	b, err := json.Marshal(m)
 	if err != nil {
@@ -70,18 +84,18 @@ func getMainFiguresList() map[string]MainFigure {
 	mainFigureMap := make(map[string]MainFigure)
 
 	// Employment
-	mainFigureMap["LF24"] = MainFigure{
-		uri:        "/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/timeseries/lf24/lms",
-		datePeriod: PERIOD_MONTHS,
-		data:       zebedee.TimeseriesMainFigure{},
-	}
+	// mainFigureMap["LF24"] = MainFigure{
+	// 	uri:        "/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/timeseries/lf24/lms",
+	// 	datePeriod: PERIOD_MONTHS,
+	// 	data:       zebedee.TimeseriesMainFigure{},
+	// }
 
 	// Unemployment
-	mainFigureMap["MGSX"] = MainFigure{
-		uri:        "/employmentandlabourmarket/peoplenotinwork/unemployment/timeseries/mgsx/lms",
-		datePeriod: PERIOD_MONTHS,
-		data:       zebedee.TimeseriesMainFigure{},
-	}
+	// mainFigureMap["MGSX"] = MainFigure{
+	// 	uri:        "/employmentandlabourmarket/peoplenotinwork/unemployment/timeseries/mgsx/lms",
+	// 	datePeriod: PERIOD_MONTHS,
+	// 	data:       zebedee.TimeseriesMainFigure{},
+	// }
 
 	// Inflation (CPIH)
 	mainFigureMap["L55O"] = MainFigure{
