9e64099e74b7fc53661f371402644cf1446d482b
Jon Dewi Jones <jondewijones@gmail.com>
2020-04-30 22:08:09 +0100

    Add test for homepage handlers

--- homepage/homepage.go
+++ homepage/homepage.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"sync"
 
-	"github.com/ONSdigital/dp-api-clients-go/renderer"
+	"github.com/ONSdigital/dp-api-clients-go/headers"
 	"github.com/ONSdigital/dp-api-clients-go/zebedee"
 	"github.com/ONSdigital/dp-frontend-homepage-controller/mapper"
 	model "github.com/ONSdigital/dp-frontend-models/model/homepage"
@@ -33,14 +33,25 @@ type RenderClient interface {
 }
 
 // Handler handles requests to homepage endpoint
-func Handler(rend renderer.Renderer, zcli *zebedee.Client) http.HandlerFunc {
+func Handler(rend RenderClient, zcli ZebedeeClient) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		handle(w, req, rend, zcli)
 	}
 }
 
-func handle(w http.ResponseWriter, req *http.Request, rend renderer.Renderer, zcli ZebedeeClient) {
+func handle(w http.ResponseWriter, req *http.Request, rend RenderClient, zcli ZebedeeClient) {
 	ctx := req.Context()
+
+	userAccessToken, err := headers.GetUserAuthToken(req)
+	if err == headers.ErrHeaderNotFound {
+		log.Event(ctx, "no user access token header set", log.ERROR, log.Error(err))
+		userAccessToken = ""
+	}
+	if err != nil {
+		log.Event(ctx, "error getting user access token from header", log.ERROR, log.Error(err))
+		userAccessToken = ""
+	}
+
 	mainFiguresList := getMainFiguresList()
 
 	var mappedMainFigures []model.MainFigure
@@ -50,7 +61,7 @@ func handle(w http.ResponseWriter, req *http.Request, rend renderer.Renderer, zc
 		wg.Add(1)
 		go func(ctx context.Context, zcli ZebedeeClient, value MainFigure) {
 			defer wg.Done()
-			zebResp, err := zcli.GetTimeseriesMainFigure(ctx, "", value.uri)
+			zebResp, err := zcli.GetTimeseriesMainFigure(ctx, userAccessToken, value.uri)
 			if err != nil {
 				log.Event(ctx, "error getting timeseries data", log.Error(err))
 				http.Error(w, "error getting timeseries data", http.StatusBadRequest)
@@ -87,9 +98,12 @@ func handle(w http.ResponseWriter, req *http.Request, rend renderer.Renderer, zc
 }
 
 const (
-	PERIOD_YEARS    = "years"
-	PERIOD_QUARTERS = "quarters"
-	PERIOD_MONTHS   = "months"
+	// PeriodYears is the string value for years time period
+	PeriodYears = "years"
+	// PeriodQuarters is the string value for quarters time period
+	PeriodQuarters = "quarters"
+	// PeriodMonths is the string value for months time period
+	PeriodMonths = "months"
 )
 
 func getMainFiguresList() map[string]MainFigure {
@@ -98,35 +112,35 @@ func getMainFiguresList() map[string]MainFigure {
 	// Employment
 	// mainFigureMap["LF24"] = MainFigure{
 	// 	uri:        "/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/timeseries/lf24/lms",
-	// 	datePeriod: PERIOD_MONTHS,
+	// 	datePeriod: PeriodMonths,
 	// 	data:       zebedee.TimeseriesMainFigure{},
 	// }
 
 	// Unemployment
 	// mainFigureMap["MGSX"] = MainFigure{
 	// 	uri:        "/employmentandlabourmarket/peoplenotinwork/unemployment/timeseries/mgsx/lms",
-	// 	datePeriod: PERIOD_MONTHS,
+	// 	datePeriod: PeriodMonths,
 	// 	data:       zebedee.TimeseriesMainFigure{},
 	// }
 
 	// Inflation (CPIH)
 	mainFigureMap["L55O"] = MainFigure{
 		uri:        "/economy/inflationandpriceindices/timeseries/l55o/mm23",
-		datePeriod: PERIOD_MONTHS,
+		datePeriod: PeriodMonths,
 		data:       zebedee.TimeseriesMainFigure{},
 	}
 
 	// GDP
 	mainFigureMap["IHYQ"] = MainFigure{
 		uri:        "/economy/grossdomesticproductgdp/timeseries/ihyq/qna",
-		datePeriod: PERIOD_QUARTERS,
+		datePeriod: PeriodQuarters,
 		data:       zebedee.TimeseriesMainFigure{},
 	}
 
 	// Population
 	mainFigureMap["UKPOP"] = MainFigure{
 		uri:        "/peoplepopulationandcommunity/populationandmigration/populationestimates/timeseries/ukpop/pop",
-		datePeriod: PERIOD_YEARS,
+		datePeriod: PeriodYears,
 		data:       zebedee.TimeseriesMainFigure{},
 	}
 
--- homepage/homepage_test.go
+++ homepage/homepage_test.go
@@ -2,14 +2,16 @@ package homepage
 
 import (
 	"context"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/ONSdigital/dp-api-clients-go/zebedee"
+	"github.com/gorilla/mux"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestUnitMapper(t *testing.T) {
-	ctx := context.Background()
 
 	var mockedZebedeeData []zebedee.TimeseriesMainFigure
 	mockedZebedeeData = append(mockedZebedeeData, zebedee.TimeseriesMainFigure{
@@ -56,12 +58,42 @@ func TestUnitMapper(t *testing.T) {
 		URI: "test/uri/timeseries/456",
 	})
 
+	expectedSuccessResponse := "<html><body><h1>Some HTML from renderer!</h1></body></html>"
+
 	Convey("test homepage handler", t, func() {
 		mockZebedeeClient := &ZebedeeClientMock{
 			GetTimeseriesMainFigureFunc: func(ctx context.Context, userAuthToken, uri string) (m zebedee.TimeseriesMainFigure, err error) {
 				return mockedZebedeeData[0], nil
 			},
 		}
+
+		mockRenderClient := &RenderClientMock{
+			DoFunc: func(string, []byte) ([]byte, error) {
+				return []byte(expectedSuccessResponse), nil
+			},
+		}
+
+		req := httptest.NewRequest("GET", "/", nil)
+		req.Header.Set("X-Florence-Token", "testuser")
+		rec := httptest.NewRecorder()
+		router := mux.NewRouter()
+		router.Path("/").HandlerFunc(Handler(mockRenderClient, mockZebedeeClient))
+
+		Convey("returns 200 response", func() {
+			router.ServeHTTP(rec, req)
+			So(rec.Code, ShouldEqual, http.StatusOK)
+		})
+
+		Convey("renderer returns HTML body", func() {
+			router.ServeHTTP(rec, req)
+			response := rec.Body.String()
+			So(response, ShouldEqual, expectedSuccessResponse)
+		})
+	})
+
+	Convey("get main figures list func returns correctly", t, func() {
+		list := getMainFiguresList()
+		So(list, ShouldNotBeEmpty)
 	})
 
 }
