b8d44eb540eb9536b4883ac48026fdc5817ef3da
Mariana Silva <Marianasilvago1234@gmail.com>
2021-06-15 23:18:51 +0530

    move clients to homepage package. stop service on first homepage caching failure.

--- routes/routes.go
+++ routes/routes.go
@@ -4,24 +4,11 @@ import (
 	"context"
 	"net/http"
 
-	"github.com/ONSdigital/dp-frontend-homepage-controller/clients/release_calendar"
-
-	"github.com/ONSdigital/dp-api-clients-go/image"
-	"github.com/ONSdigital/dp-api-clients-go/renderer"
-	"github.com/ONSdigital/dp-api-clients-go/zebedee"
 	"github.com/ONSdigital/dp-frontend-homepage-controller/homepage"
 
 	"github.com/ONSdigital/log.go/log"
 	"github.com/gorilla/mux"
 )
-
-// Clients contains all the required Clients for frontend homepage controller
-type Clients struct {
-	Renderer *renderer.Renderer
-	Zebedee  *zebedee.Client
-	Babbage  *release_calendar.Client
-	ImageAPI *image.Client
-}
 
 // Init initialises routes for the service
 func Init(ctx context.Context, r *mux.Router, hcHandler func(http.ResponseWriter, *http.Request), homepageClient homepage.HomepageClienter) {
