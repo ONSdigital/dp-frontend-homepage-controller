f2ea181b4dcc03202f3a9cbff1e231e87f5af41c
janderson2 <j.anderson.ons@gmail.com>
2022-02-18 11:18:02 +0000

    Remove release calendar logic
    Remove the release calendar logic as the final iteration of the homepage
    only includes a link to the release calendar. This change removes the
    dependency on babbage thereby making the homepage controller more
    resilient due to having fewer dependencies and therefore fewer
    opportunities for failure.

--- homepage/clients.go
+++ homepage/clients.go
@@ -2,22 +2,18 @@ package homepage
 
 import (
 	"context"
+
 	"github.com/ONSdigital/dp-api-clients-go/v2/image"
 	"github.com/ONSdigital/dp-api-clients-go/v2/zebedee"
-	"github.com/ONSdigital/dp-frontend-homepage-controller/clients/release_calendar"
 	health "github.com/ONSdigital/dp-healthcheck/healthcheck"
 )
+
+//go:generate moq -out mocks_test.go -pkg homepage . ZebedeeClient ImageClient RenderClient
 
 // ZebedeeClient is an interface with methods required for a zebedee client
 type ZebedeeClient interface {
 	GetTimeseriesMainFigure(ctx context.Context, userAuthToken, collectionID, lang, uri string) (m zebedee.TimeseriesMainFigure, err error)
 	GetHomepageContent(ctx context.Context, userAccessToken, collectionID, lang, path string) (m zebedee.HomepageContent, err error)
-	Checker(ctx context.Context, check *health.CheckState) error
-}
-
-// BabbageClient is an interface with methods required for a babbage client
-type BabbageClient interface {
-	GetReleaseCalendar(ctx context.Context, userAccessToken, fromDay, fromMonth, fromYear string) (m release_calendar.ReleaseCalendar, err error)
 	Checker(ctx context.Context, check *health.CheckState) error
 }
 
@@ -37,6 +33,5 @@ type RenderClient interface {
 type Clients struct {
 	Renderer RenderClient
 	Zebedee  ZebedeeClient
-	Babbage  BabbageClient
 	ImageAPI ImageClient
 }
--- service/service_test.go
+++ service/service_test.go
@@ -123,10 +123,9 @@ func TestRun(t *testing.T) {
 				So(err, ShouldNotBeNil)
 				So(err.Error(), ShouldResemble, fmt.Sprintf("unable to register checkers: %s", errAddheckFail.Error()))
 				So(svcList.HealthCheck, ShouldBeTrue)
-				So(len(hcMockAddFail.AddCheckCalls()), ShouldEqual, 3)
+				So(len(hcMockAddFail.AddCheckCalls()), ShouldEqual, 2)
 				So(hcMockAddFail.AddCheckCalls()[0].Name, ShouldResemble, "frontend renderer")
-				So(hcMockAddFail.AddCheckCalls()[1].Name, ShouldResemble, "Babbage")
-				So(hcMockAddFail.AddCheckCalls()[2].Name, ShouldResemble, "API router")
+				So(hcMockAddFail.AddCheckCalls()[1].Name, ShouldResemble, "API router")
 			})
 		})
 
@@ -147,10 +146,9 @@ func TestRun(t *testing.T) {
 			})
 
 			Convey("The checkers are registered and the healthcheck and http server started", func() {
-				So(len(hcMock.AddCheckCalls()), ShouldEqual, 3)
+				So(len(hcMock.AddCheckCalls()), ShouldEqual, 2)
 				So(hcMock.AddCheckCalls()[0].Name, ShouldResemble, "frontend renderer")
-				So(hcMock.AddCheckCalls()[1].Name, ShouldResemble, "Babbage")
-				So(hcMock.AddCheckCalls()[2].Name, ShouldResemble, "API router")
+				So(hcMock.AddCheckCalls()[1].Name, ShouldResemble, "API router")
 				So(len(initMock.DoGetHTTPServerCalls()), ShouldEqual, 1)
 				So(initMock.DoGetHTTPServerCalls()[0].BindAddr, ShouldEqual, ":24400")
 				So(len(hcMock.StartCalls()), ShouldEqual, 1)
