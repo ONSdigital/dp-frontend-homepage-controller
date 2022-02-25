364dc63922523e2cd49154fd9218e4798e85a9a6
Noelle <noelle.legrain@gmail.com>
2022-02-25 15:53:30 +0000

    update test

--- config/config_test.go
+++ config/config_test.go
@@ -23,7 +23,7 @@ func TestSpec(t *testing.T) {
 				So(cfg.GracefulShutdownTimeout, ShouldEqual, 5*time.Second)
 				So(cfg.HealthCheckInterval, ShouldEqual, 30*time.Second)
 				So(cfg.HealthCheckCriticalTimeout, ShouldEqual, 90*time.Second)
-				So(cfg.PatternLibraryAssetsPath, ShouldEqual, "//cdn.ons.gov.uk/sixteens/f80be2c")
+				So(cfg.PatternLibraryAssetsPath, ShouldEqual, "//cdn.ons.gov.uk/sixteens/77f1d9b")
 				So(cfg.IsPublishingMode, ShouldEqual, false)
 				So(cfg.CacheUpdateInterval, ShouldEqual, 10*time.Second)
 				So(cfg.Languages, ShouldEqual, "en,cy")
