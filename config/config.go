ce11ef9a72463b0731df0a00d291401fc5a56166
Noelle <noelle.legrain@gmail.com>
2022-06-22 14:11:56 +0100

    update about the census link

--- config/config.go
+++ config/config.go
@@ -58,7 +58,7 @@ func get() (*Config, error) {
 		IsPublishingMode:           false,
 		Languages:                  "en,cy",
 		SiteDomain:                 "localhost",
-		CensusFirstResults:         "Wednesday, 27-Jul-22 14:29:00 UTC",
+		CensusFirstResults:         "Tuesday, 21-Jun-22 14:29:00 UTC",
 	}
 
 	return cfg, envconfig.Process("", cfg)
