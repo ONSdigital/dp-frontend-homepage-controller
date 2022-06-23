d9026749ab56b11f058080cf68a0f2d0adc5be49
Noelle <noelle.legrain@gmail.com>
2022-06-23 14:17:07 +0100

    update test

--- config/config_test.go
+++ config/config_test.go
@@ -28,7 +28,7 @@ func TestSpec(t *testing.T) {
 				So(cfg.CacheUpdateInterval, ShouldEqual, 10*time.Second)
 				So(cfg.Languages, ShouldEqual, "en,cy")
 				So(cfg.SiteDomain, ShouldEqual, "localhost")
-				So(cfg.CensusFirstResults, ShouldEqual, "Wednesday, 27-Jul-22 14:29:00 UTC")
+				So(cfg.CensusFirstResults, ShouldEqual, "Wednesday, 27-Jul-22 11:00:00 BST")
 			})
 		})
 	})
