25804e75d39e1a978450fb6b9c24fa63a2ccbfc6
Noelle <noelle.legrain@gmail.com>
2023-03-28 12:01:06 +0100

    update links

--- config/config.go
+++ config/config.go
@@ -65,7 +65,7 @@ func get() (*Config, error) {
 		CacheUpdateInterval:            10 * time.Second,
 		CensusTopicID:                  "4445",
 		Debug:                          false,
-		EnableCensusTopicSubsection:    false,
+		EnableCensusTopicSubsection:    true,
 		EnableCustomDataset:            false,
 		EnableGetDataCard:              false,
 		EnableNewNavBar:                false,
