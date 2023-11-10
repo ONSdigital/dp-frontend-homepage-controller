80fa7a95e45dcb1f5a161c0a693e2bcf84968ae0
Joshua Singh <joshua.singh@ons.gov.uk>
2023-11-10 16:52:52 +0000

    Rewrote conditional

--- homepage/homepage.go
+++ homepage/homepage.go
@@ -92,33 +92,42 @@ func init() {
 
 	mainFigureMap = make(map[string]MainFigure)
 
-	mainFigureMap["emp"] = MainFigure{
-		uris:               []string{"/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/timeseries/lf24/lms"},
-		trendURI:           "/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/timeseries/fux7/lms",
-		datePeriod:         mapper.PeriodMonth,
-		data:               zebedee.TimeseriesMainFigure{},
-		differenceInterval: mapper.PeriodYear,
-	}
-
-	// Unemployment
-	mainFigureMap["unemp"] = MainFigure{
-		uris:               []string{"/employmentandlabourmarket/peoplenotinwork/unemployment/timeseries/mgsx/lms"},
-		trendURI:           "/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/timeseries/fuu8/lms",
-		datePeriod:         mapper.PeriodMonth,
-		data:               zebedee.TimeseriesMainFigure{},
-		differenceInterval: mapper.PeriodYear,
-	}
-
 	if cfg.EnableUpdatedMainFigures {
-		mainFigureMapCopy := mainFigureMap["emp"]
-		mainFigureMapCopy.uris = []string{"/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/timeseries/s2pw/lms"}
-		mainFigureMapCopy.trendURI = "/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/timeseries/s2qm/lms"
-		mainFigureMap["emp"] = mainFigureMapCopy
+		// Employment
+		mainFigureMap["emp"] = MainFigure{
+			uris:               []string{"/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/timeseries/s2pw/lms"},
+			trendURI:           "/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/timeseries/s2qm/lms",
+			datePeriod:         mapper.PeriodMonth,
+			data:               zebedee.TimeseriesMainFigure{},
+			differenceInterval: mapper.PeriodYear,
+		}
 
-		mainFigureMapCopy = mainFigureMap["unemp"]
-		mainFigureMapCopy.uris = []string{"/employmentandlabourmarket/peoplenotinwork/unemployment/timeseries/s2pu/lms"}
-		mainFigureMapCopy.trendURI = "/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/timeseries/s2qk/lms"
-		mainFigureMap["unemp"] = mainFigureMapCopy
+		// Unemployment
+		mainFigureMap["unemp"] = MainFigure{
+			uris:               []string{"/employmentandlabourmarket/peoplenotinwork/unemployment/timeseries/s2pu/lms"},
+			trendURI:           "/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/timeseries/s2qk/lms",
+			datePeriod:         mapper.PeriodMonth,
+			data:               zebedee.TimeseriesMainFigure{},
+			differenceInterval: mapper.PeriodYear,
+		}
+	} else {
+		// Employment
+		mainFigureMap["emp"] = MainFigure{
+			uris:               []string{"/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/timeseries/lf24/lms"},
+			trendURI:           "/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/timeseries/fux7/lms",
+			datePeriod:         mapper.PeriodMonth,
+			data:               zebedee.TimeseriesMainFigure{},
+			differenceInterval: mapper.PeriodYear,
+		}
+
+		// Unemployment
+		mainFigureMap["unemp"] = MainFigure{
+			uris:               []string{"/employmentandlabourmarket/peoplenotinwork/unemployment/timeseries/mgsx/lms"},
+			trendURI:           "/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/timeseries/fuu8/lms",
+			datePeriod:         mapper.PeriodMonth,
+			data:               zebedee.TimeseriesMainFigure{},
+			differenceInterval: mapper.PeriodYear,
+		}
 	}
 
 	// Inflation (CPIH)
